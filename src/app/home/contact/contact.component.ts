0079a2be45cab5f39abccf266a99ab24453b85b1
NewennT <newenn.turbiau@gmail.com>
2025-12-17 17:29:39 +0100

    Reactive Form

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { CoursesService } from '../courses/courses.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { tap, map, filter, distinctUntilChanged  } from 'rxjs';
+import { CourseCardComponent } from '../courses/course-card/course-card.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CourseCardComponent,
+    AsyncPipe,
+
+  ],
   styleUrls: ['contact.component.scss'],
   templateUrl: './contact.component.html',
 })
 export class ContactComponent {
+  private formBuilder = inject(FormBuilder);
+  private coursesService = inject(CoursesService);
+  contactForm = this.formBuilder.group({
+    name: [''],
+    email: [''],
+    course: [''],
+    message: ['']
+  });
+
+  selectedCourse$ = this.contactForm.valueChanges.pipe(
+    map(form => form.course),
+    filter(course => course !== ''),
+    distinctUntilChanged(),
+    map(course => this.coursesService.getCourseByType(course)),
+  );
+
+  onSubmitForm(): void{
+    console.log(this.contactForm.value);
+  }
 
 }
